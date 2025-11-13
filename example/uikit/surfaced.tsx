c33521e092ad733eb8efb073e2b1f160bb61dead
ilias bhallil <ilias.bhal@gmail.com>
2025-11-13 15:17:19 +0700

    remove exports in package.json

--- example/uikit/surfaced.tsx
+++ example/uikit/surfaced.tsx
@@ -1,11 +1,12 @@
-import { createSurfaced, createTextBase, createViewBase } from "surfacekit";
-
+import { createSurfaced } from "surfacekit";
 import * as themes from './theme';
 export * as themes from './theme';
 
 type Theme = typeof themes[keyof typeof themes];
 
 export const surfaced = createSurfaced<Theme>()
+export const View = surfaced.createView();
+export const Text = surfaced.createText();
 
-export const View = createViewBase(surfaced);
-export const Text = createTextBase(surfaced);
+
+<View backgroundColor="surfaces.0" />
